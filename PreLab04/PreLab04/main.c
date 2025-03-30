dcf15d7d913ab2427d5ffd9356c0896139adc922
AESC-2004 <esc22235@uvg.edu.gt>
2025-03-30 12:24:25 -0600

    ¡Un pequeño error corregido!

--- PreLab04/PreLab04/main.c
+++ PreLab04/PreLab04/main.c
@@ -157,6 +157,7 @@ ISR(PCINT0_vect)
 		{
 			COUNTUP_LAST = 1;
 			COUNT++;
+			if (COUNT > 15) COUNT = 0;
 		}
 	} else if ((PINB & ((1 << COUNTUP))) == (0 << COUNTUP))
 	{
@@ -174,6 +175,7 @@ ISR(PCINT0_vect)
 		{
 			COUNTDWN_LAST = 1;
 			COUNT--;
+			if (COUNT == 0xFF) COUNT = 15;
 		}
 	} else if ((PINB & ((1 << COUNTDWN))) == (0 << COUNTDWN))
 	{
